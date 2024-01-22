0d7df44374674354f5abdf97637435eb6042c950
David Woo <wcj617@gmail.com>
2024-01-21 20:16:59 -0800

    initial

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withPWA = require('next-pwa')({
+    dest: 'public',
+    // Enable additional route caching when users navigate through pages with next/link.
+    cacheOnFrontEndNav: true,
+    reloadOnOnline: true,
+    disable: false,
+})
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+
+}
+
+module.exports = withPWA(nextConfig);
