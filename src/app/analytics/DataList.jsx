a9cdf75440deb96df3c7cc31e411bad1f6ecf357
David Woo <wcj617@gmail.com>
2024-03-03 22:44:26 -0800

    Initial analytic pages

--- src/app/analytics/DataList.jsx
+++ src/app/analytics/DataList.jsx
@@ -1,15 +1,85 @@
-export default async function DataList({ item }){
-    // const { item } = data;
-    const { TaskName, UserId, EndTime, StartTime } = item.item.Item;
+'use client'
+// import {useMemo} from 'react';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+
+import { faker } from '@faker-js/faker';
+export default function DataList({ item }){
+
+    const taskData = item.item.Item;
+    const duration = (new Date(taskData.EndTime) - new Date(taskData.StartTime)) / 60000;
+    // console.log("faker value" + faker.helpers.rangeToNumber({min: 0, max: 1000}));
+    // const labels = ['Jan'];
+    const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    const chartData = {
+        labels,
+        datasets : [
+            {
+                label: 'Dataset 1',
+                data: labels.map(()=> duration),
+                // data: 120,
+                borderColor: 'rgb(255, 99, 132)',
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            },
+            {
+                label: 'Dataset 2',
+                data: labels.map(() => faker.helpers.rangeToNumber({min: 0, max: 1000})),
+                borderColor: 'rgb(53, 162, 235)',
+                backgroundColor: 'rgba(53, 162, 235, 0.5)',
+            },
+        ],
+    };
+
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Task tracker',
+            },
+
+        },
+        scales: {
+            y: {
+                ticks: {
+                    // Include a dollar sign in the ticks
+                    callback: function(value, index, ticks) {
+                        const totalMinutes = value;
+                        const hours = Math.floor(totalMinutes / 60);
+                        const minutes = totalMinutes % 60;
+                        return `${hours}h ${minutes}m`
+                    }
+                }
+            }
+        }
+    };
 
     return (
         <>
-            <h1>Testing</h1>
-            <div>Task Name: {TaskName}</div>
-            <div>User ID: {UserId}</div>
-            <div>Start Time: {StartTime} </div>
-            <div>End Time: {EndTime} </div>
-            <h1>Testing</h1>
+            <Line options={options} data={chartData}  />;
         </>
     )
 }
